2fef8b0e0a629f9a200dc95e0ea488ff1e39b333
Rafael <rafa.comp@gmail.com>
2019-05-25 11:00:24 -0300

    configuracao inicial do player

--- src/pages/Podcast/index.js
+++ src/pages/Podcast/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PlayerActions from '~/store/ducks/player';
+
 import {
   Container,
   EpisodeList,
@@ -17,10 +21,17 @@ import {
   BackButton,
 } from './styles';
 
-export default class Podcast extends Component {
+class Podcast extends Component {
   handleBack = () => {
     const { navigation } = this.props;
     navigation.goBack();
+  };
+
+  handlePlay = (episodeId) => {
+    const { setPodcastRequest, navigation } = this.props;
+    const podcast = navigation.getParam('podcast');
+
+    setPodcastRequest(podcast, episodeId);
   };
 
   render() {
@@ -38,7 +49,7 @@ export default class Podcast extends Component {
               <Cover source={{ uri: podcast.cover }} />
 
               <PodcastTitle>{podcast.title}</PodcastTitle>
-              <PlayButton onPress={() => {}}>
+              <PlayButton onPress={() => this.handlePlay()}>
                 <PlayButtonText>Reproduzir</PlayButtonText>
               </PlayButton>
             </PodcastDetails>
@@ -46,7 +57,7 @@ export default class Podcast extends Component {
           data={podcast.tracks}
           keyExtractor={episode => String(episode.id)}
           renderItem={({ item: episode }) => (
-            <Episode>
+            <Episode onPress={() => this.handlePlay(episode.id)}>
               <Title>{episode.title}</Title>
               <Author>{episode.artist}</Author>
             </Episode>
@@ -56,3 +67,10 @@ export default class Podcast extends Component {
     );
   }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(Podcast);
